65e862a82613baa78f70bd82eb22116cb93be831
Brice Brinson <26476112+blbrinson@users.noreply.github.com>
2020-05-19 11:05:07 -0500

    Add files via upload

---
+++ 1 - I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Brice on 4/14/20.
+//  Copyright © 2020 blbrinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
